57e364b530245b50985b38dd8123be11ad7acf4d
Héctor Gracia <whinedo@gmail.com>
2017-05-21 20:48:56 +0200

    added web and rest controller for thymeleaf example and get all teams

--- examples/lab1-simple-web-app/lab1/src/main/java/es/whinedo/pilots/cloud/lab1/Lab1Application.java
+++ examples/lab1-simple-web-app/lab1/src/main/java/es/whinedo/pilots/cloud/lab1/Lab1Application.java
@@ -1,12 +1,41 @@
 package es.whinedo.pilots.cloud.lab1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import es.whinedo.pilots.cloud.lab1.model.Team;
+import es.whinedo.pilots.cloud.lab1.repositories.ITeamRepository;
 
 @SpringBootApplication
 public class Lab1Application {
 
+	@Autowired
+	private ITeamRepository teamRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
 	}
+	
+	@PostConstruct
+	public void init() {
+	  	List<Team> list = new ArrayList<>();
+
+	  	Team team = new Team();
+	  	team.setLocation("Harlem");
+	  	team.setName("Globetrotters");
+	  	list.add(team);
+	  	
+	  	team = new Team();
+	  	team.setLocation("Washington");
+	  	team.setName("Generals");
+	  	list.add(team);
+
+	  	teamRepository.save(list);
+	  }   
 }
