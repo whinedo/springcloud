ac57e0e427839005bf4679ebc12e672dd3ecb2b5
Héctor Gracia <whinedo@gmail.com>
2017-06-06 22:14:19 +0200

    added subjer for eureka lab-4 initial structure

---
+++ examples/lab-4-subject/src/main/java/es/whinedo/pilots/cloud/lab4subject/Lab4SubjectApplication.java
@@ -0,0 +1,12 @@
+package es.whinedo.pilots.cloud.lab4subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4SubjectApplication.class, args);
+	}
+}
