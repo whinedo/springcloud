498bc25a8f15f6639934ed5a29fd8c0d6d996ada
Héctor Gracia <whinedo@gmail.com>
2017-06-06 22:06:38 +0200

    added eureka server initial structure

---
+++ examples/lab-4-eureka-server/src/main/java/es/whinedo/pilots/cloud/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package es.whinedo.pilots.cloud.lab4eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+}
--- examples/lab2-server/src/main/java/es/whinedo/pilots/cloud/lab2/Lab2Application.java
+++ examples/lab2-server/src/main/java/es/whinedo/pilots/cloud/lab2/Lab2Application.java
@@ -2,8 +2,10 @@ package es.whinedo.pilots.cloud.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Lab2Application {
 
 	public static void main(String[] args) {
