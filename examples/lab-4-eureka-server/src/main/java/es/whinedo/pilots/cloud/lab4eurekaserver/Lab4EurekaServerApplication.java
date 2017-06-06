62b81d5972bff15b9b01fb8c7abc3e5fea7375e9
Héctor Gracia <whinedo@gmail.com>
2017-06-06 22:13:47 +0200

    added and configured eureka server

--- examples/lab-4-eureka-server/src/main/java/es/whinedo/pilots/cloud/lab4eurekaserver/Lab4EurekaServerApplication.java
+++ examples/lab-4-eureka-server/src/main/java/es/whinedo/pilots/cloud/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -2,8 +2,10 @@ package es.whinedo.pilots.cloud.lab4eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Lab4EurekaServerApplication {
 
 	public static void main(String[] args) {
