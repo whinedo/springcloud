f9c1ff353ff1c0961fdfd2bf2ff4077727acf02a
Héctor Gracia <whinedo@gmail.com>
2017-06-06 22:21:14 +0200

    added subject logic

--- examples/lab-4-subject/src/main/java/es/whinedo/pilots/cloud/lab4subject/Lab4SubjectApplication.java
+++ examples/lab-4-subject/src/main/java/es/whinedo/pilots/cloud/lab4subject/Lab4SubjectApplication.java
@@ -2,8 +2,10 @@ package es.whinedo.pilots.cloud.lab4subject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Lab4SubjectApplication {
 
 	public static void main(String[] args) {
