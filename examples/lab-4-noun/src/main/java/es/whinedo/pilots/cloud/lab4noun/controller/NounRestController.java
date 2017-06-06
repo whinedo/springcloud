fdb7a4c147e27fc7aa4b7b22322e30664db1d9e9
Héctor Gracia <whinedo@gmail.com>
2017-06-06 22:38:02 +0200

    added lab-4-noun structure and login

---
+++ examples/lab-4-noun/src/main/java/es/whinedo/pilots/cloud/lab4noun/controller/NounRestController.java
@@ -0,0 +1,21 @@
+package es.whinedo.pilots.cloud.lab4noun.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NounRestController {
+
+	@Value("${words}")
+	private String words;
+
+	@RequestMapping("/")
+	public @ResponseBody String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+
+}
