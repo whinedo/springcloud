bc10a16717a6442ac1c801a9e34d420d2c35e779
Héctor Gracia <whinedo@gmail.com>
2017-06-18 22:34:23 +0200

    forgotten files

---
+++ examples/lab2-config-server/src/main/java/es/whinedo/pilots/cloud/lab2/config/Lab2Application.java
@@ -0,0 +1,14 @@
+package es.whinedo.pilots.cloud.lab2.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Application.class, args);
+	}
+}
